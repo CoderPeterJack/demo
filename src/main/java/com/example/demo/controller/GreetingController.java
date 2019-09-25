9ffa089d8f64de1f271dbf27e1caa028bf491c9a
JiangPeng <1179191373@qq.com>
2019-09-25 21:36:55 +0800

    init repo

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/hello")
+    public String greeting(){
+        return "Hello World";
+    }
+
+
+}
